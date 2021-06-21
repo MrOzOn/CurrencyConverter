efa7adecb3a2603725a7afe7d25747f0baab933b
MrOzOn <andrew_1982>
2021-06-21 21:49:00 +0300

    add Timber add repository for update db from network

--- app/src/main/java/com/mrozon/currencyconverter/MyApp.kt
+++ app/src/main/java/com/mrozon/currencyconverter/MyApp.kt
@@ -2,6 +2,18 @@ package com.mrozon.currencyconverter
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
-class MyApp: Application()
+class MyApp: Application() {
+    override fun onCreate() {
+        super.onCreate()
+        initTimber()
+    }
+
+    private fun initTimber() {
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+}
